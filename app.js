8a33cf8d54dcfedb669e3d15854e58301e2a1b93
AleksandarStanojevic1509 <aleksandar.stanojevic.1509@gmail.com>
2020-04-27 01:31:52 +0200

    edit css & js

--- app.js
+++ app.js
@@ -3,65 +3,69 @@ const totalDeaths = document.querySelector(".total-deaths h2");
 const totalRecovered = document.querySelector(".total-recovered h2");
 const totalActive = document.querySelector(".active-cases h2");
 const lastUpdate = document.querySelectorAll(".last-up");
-const tbody = document.querySelector ("tbody")
+const tbody = document.querySelector("tbody");
 
-let lastUp= (d) => {
+let lastUp = (d) => {
   let date = new Date(d);
-    let day = date.getDate();
-    let year = date.getFullYear();
-    let month = date.getMonth();
-    let hour = date.getHours();
-    let minutes = date.getMinutes();
+  let day = date.getDate();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
 
-   return `${year}-${month + 1}-${day} ${hour}:${minutes}`;
-}
-
+  return `${year}-${month + 1}-${day} ${hour}:${minutes}`;
+};
 
 fetch("https://covid19.mathdro.id/api")
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    console.log(data);
-
-    let confirmed = data.confirmed.value;
-    let deaths = data.deaths.value;
-    let recovered = data.recovered.value;
-    let active = confirmed - recovered - deaths;
-
-    totalCases.textContent = confirmed.toLocaleString();
-    totalDeaths.textContent = deaths.toLocaleString();
-    totalRecovered.textContent = recovered.toLocaleString();
-    totalActive.textContent = active.toLocaleString();
+    totalCases.textContent = data.confirmed.value.toLocaleString();
+    totalDeaths.textContent = data.deaths.value.toLocaleString();
+    totalRecovered.textContent = data.recovered.value.toLocaleString();
+    totalActive.textContent = (
+      data.confirmed.value -
+      data.deaths.value -
+      data.recovered.value
+    ).toLocaleString();
     lastUpdate.forEach((l) => {
       l.textContent = lastUp(data.lastUpdate);
     });
-    // console.log(data.recovered);
-    // console.log(data.lastUpdate);
     return data.confirmed.detail;
+  })
+  .catch(err=>{
+    console.error(err)
   });
 
-
-  fetch('https://covid19.mathdro.id/api/countries')
-  .then(response =>{
-    return response.json()
+fetch("https://covid19.mathdro.id/api/countries")
+  .then((response) => {
+    return response.json();
   })
-  .then(data =>{
-    data.countries.forEach(e=>{
+  .then((data) => {
+    data.countries.forEach((e) => {
       fetch(`https://covid19.mathdro.id/api/countries/${e.name}`)
-      .then(response =>{
-        return response.json()
-      })
-      .then(data =>{
-        console.log(data)
-        tbody.innerHTML += `<td>${e.name}</td>
-        <td class="total-cases" >${data.confirmed.value}</td>
-        <td class="total-deaths">${data.deaths.value}</td>
-        <td class="total-recovered">${data.recovered.value}</td>
-        <td class="active-cases">${data.confirmed.value-data.deaths.value-data.recovered.value}</td>
-        <td>${lastUp(data.lastUpdate)}</td>`
-        
-      })
-      
-    })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          // console.log(data);
+          tbody.innerHTML += `<td>${e.name}</td>
+        <td class="total-cases" >${data.confirmed.value.toLocaleString()}</td>
+        <td class="total-deaths">${data.deaths.value.toLocaleString()}</td>
+        <td class="total-recovered">${data.recovered.value.toLocaleString()}</td>
+        <td class="active-cases">${(
+          data.confirmed.value -
+          data.deaths.value -
+          data.recovered.value
+        ).toLocaleString()}</td>
+        <td>${lastUp(data.lastUpdate)}</td>`;
+        })
+        .catch(err=>{
+          console.error(err)
+        });
+    });
   })
+  .catch(err=>{
+    console.error(err)
+  });
